7336240b0bd07805b657a7266e9190da98b0a694
lucEvent <rafaa89@gmail.com>
2014-10-24 16:57:57 +0200

    0.0.2 No funciona todavia

--- src/_1_xifrar_desxifrar/AES.java
+++ src/_1_xifrar_desxifrar/AES.java
@@ -5,7 +5,16 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
 /**
@@ -13,31 +22,53 @@ import javax.crypto.spec.SecretKeySpec;
  */
 public class AES {
 
-    private Debug debug;
-  //  private String key = "thisisasecretkey";
-    
+    private Debug2 debug2;
+    //  private String key = "thisisasecretkey";
+    private SecretKey aesKey;
+
     public AES() {
-        debug = new Debug();
+
+        debug2 = new Debug2();
+
+        KeyGenerator keygen;
+        try {
+            keygen = KeyGenerator.getInstance("AES");
+            aesKey = keygen.generateKey();
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("AES.NoSuchAlgorithmException_1 " + e.toString());
+
+        }
+
     }
-    
+
     public String xifrar(File fitxer, String clauXifrat) throws FileNotFoundException, IOException {
         return xifrar(llegirFitxer(fitxer), clauXifrat);
     }
 
     public String xifrar(String fitxer, String clauXifrat) {
 
-        String res = debug.encrypt(clauXifrat, fitxer);
+
+
+
+        return null;
+    }
+
+    public byte[] xifrar_b(File fitxer, String clauXifrat) throws FileNotFoundException, IOException {
+        return xifrar_b(llegirFitxer(fitxer), clauXifrat);
+    }
+
+    public byte[] xifrar_b(String fitxer, String clauXifrat) {
+
+        byte[] res = debug2.encrypt(aesKey, fitxer);
+
 
         return res;
     }
 
-    public String desxifrar(File fitxerenc, String clauXifrat) throws FileNotFoundException, IOException {
-        return desxifrar(llegirFitxer(fitxerenc), clauXifrat);
-    }
+    public String desxifrar(byte[] fitxerenc) {
 
-    public String desxifrar(String fitxerenc, String clauXifrat) {
-
-        String res = debug.decrypt(clauXifrat, fitxerenc);
+        //       String res = debug.decrypt(clauXifrat, fitxerenc);
+        String res = debug2.decrypt(aesKey, fitxerenc);
 
         return res;
     }
@@ -54,39 +85,135 @@ public class AES {
         return content.toString();
     }
 
-    private class Debug {
+    class Debug2 {
 
-        String algorithm = "AES";
+        byte[] encrypt(SecretKey aesKey, String text) {
 
-        String encrypt(String key, String value) {
+
+
+            Cipher aesCipher;
             try {
-
-                
-                SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes(), algorithm);
-                Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-                cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
-                byte[] encrypted = cipher.doFinal(value.getBytes());
-                System.out.println("encrypted string:" + (new String(encrypted)));
-                System.out.println("skeySpec.getEncoded:" + (new String(skeySpec.getEncoded())));
-                return new String(skeySpec.getEncoded());
-            } catch (Exception ex) {
-                ex.printStackTrace();
+                // Create the cipher
+                aesCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            } catch (NoSuchAlgorithmException e) {
+                System.out.println("Encrypt.NoSuchAlgorithmException_2 " + e.toString());
+                return null;
+            } catch (NoSuchPaddingException e) {
+                System.out.println("Encrypt.NoSuchPaddingException " + e.toString());
+                return null;
             }
-            return null;
+            //We use the generated aesKey from above to initialize the Cipher object for encryption:
+
+            try {
+                // Initialize the cipher for encryption
+                aesCipher.init(Cipher.ENCRYPT_MODE, aesKey);
+            } catch (InvalidKeyException e) {
+                System.out.println("Encrypt.InvalidKeyException " + e.toString());
+                return null;
+            }
+
+            // Our cleartext
+            byte[] cleartext = text.getBytes();
+
+            // Encrypt the cleartext
+            byte[] ciphertext;
+            try {
+                ciphertext = aesCipher.doFinal(cleartext);
+            } catch (IllegalBlockSizeException e) {
+                System.out.println("Encrypt.IllegalBlockSizeException " + e.toString());
+                return null;
+            } catch (BadPaddingException e) {
+                System.out.println("Encrypt.BadPaddingException " + e.toString());
+                return null;
+            }
+
+
+
+
+            return ciphertext;
         }
 
-        public String decrypt(String key, String encrypted) {
-            try {
-                SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes(), algorithm);
-                Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-                cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(skeySpec.getEncoded(), algorithm));
+        public String decrypt(SecretKey aesKey, String encryptedtext) {
+            return decrypt(aesKey, encryptedtext.getBytes());
+        }
 
-                byte[] original = cipher.doFinal(encrypted.getBytes());
-                original.toString();
-            } catch (Exception ex) {
-                ex.printStackTrace();
+        public String decrypt(SecretKey aesKey, byte[] encryptedbytes) {
+
+            Cipher aesCipher;
+            try {
+                // Create the cipher
+                aesCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            } catch (NoSuchAlgorithmException e) {
+                System.out.println("Decrypt.NoSuchAlgorithmException_2 " + e.toString());
+                return null;
+            } catch (NoSuchPaddingException e) {
+                System.out.println("Decrypt.NoSuchPaddingException " + e.toString());
+                return null;
             }
-            return null;
+
+            //We use the generated aesKey from above to initialize the Cipher object for encryption:
+            try {
+                // Initialize the same cipher for decryption
+                aesCipher.init(Cipher.DECRYPT_MODE, aesKey);
+            } catch (InvalidKeyException e) {
+                System.out.println("Decrypt.InvalidKeyException " + e.toString());
+                return null;
+            }
+            System.out.println("Descifrando encryptedbytes:" + new String(encryptedbytes));
+            System.out.println("Descifrando aesCipher" + aesCipher.toString());
+            byte[] cleartext;
+            try {
+                // Decrypt the ciphertext
+                cleartext = aesCipher.doFinal(encryptedbytes);
+            } catch (IllegalBlockSizeException e) {
+                System.out.println("Decrypt.IllegalBlockSizeException " + e.toString());
+                return null;
+            } catch (BadPaddingException e) {
+                System.out.println("Decrypt.BadPaddingException " + e.toString());
+                return null;
+            }
+            System.out.println("->" + new String(cleartext) + "<-->" + new String(encryptedbytes) + "<-");
+
+            return new String(cleartext);
+
+
+        }
+
+        class Debug3 {
+
+            public byte[] encrypt(SecretKey key, byte[] content) {
+                byte[] ciphertext = null;
+                try {
+                    Cipher aesCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+                    aesCipher.init(Cipher.ENCRYPT_MODE, aesKey);
+
+
+                    ciphertext = aesCipher.doFinal(content);
+                } catch (Exception ex) {
+                    Logger.getLogger(AES.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                return ciphertext;
+            }
+
+            public byte[] decrypt(SecretKey key, byte[] content) {
+                byte[] cleartext1;
+                try {
+                    Cipher aesCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+    
+      aesCipher.init(Cipher.DECRYPT_MODE, aesKey);
+
+      // Decrypt the ciphertext
+      cleartext1 = aesCipher.doFinal(content);
+      
+      
+      return cleartext1;
+                } catch (NoSuchAlgorithmException ex) {
+                    Logger.getLogger(AES.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (NoSuchPaddingException ex) {
+                    Logger.getLogger(AES.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+            }
         }
     }
 }
--- src/crypto/Crypto.java
+++ src/crypto/Crypto.java
@@ -1,20 +1,22 @@
 package crypto;
 
 import _1_xifrar_desxifrar.AES;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 
 /**
  * @author rafael.jose.lucena
  */
 public class Crypto {
-    
+
     private static final int XIFRAR = 0;
     private static final int DESXIFRAR = 1;
-    
-    
+
     public static void main(String[] args) {
         AES p1 = new AES();
-        
-        if (args.length < 4) {
+
+        if (args.length < 4 && false) {
             System.out.println("/*************** USAGE ***************/");
             System.out.println("/*                                   */");
             System.out.println("/**/");
@@ -25,27 +27,80 @@ public class Crypto {
             System.out.println("/**/");
             System.out.println("/*************************************/");
         }
-        
+
         String message;// = args[1];
         String key;// = Long.parseLong(args[2]);
         int option;// = Integer.parseInt(args[3]);
-        
+
         message = "Minamessageplease";
         key = "123456789";
+
+        //Debug
         
-   
-        option = XIFRAR;
+        example();
+        
+        System.out.println("");
+        String m = "Este deberia ser el mensaje correcto";
+        //Cifrar
+        byte[] m_cifrado = p1.xifrar_b("", m);
+        System.out.println("Mensage inicial: " + m);
+        System.out.println("Mensage cifrado: " + new String(m_cifrado));
+
+        //Descifrar
+        String m_descifrado = p1.desxifrar(m_cifrado);
+
+        System.out.println("Mensage descifr: " + m_descifrado + " size:" + m_descifrado.length());
+
+        if (true) {
+            return;
+        }
+        //End debug
+
+
+
+        option = DESXIFRAR;
         String res;
-        switch(option) {
+        switch (option) {
             case XIFRAR:
-                res = p1.xifrar(message, key);
+                //     res = p1.xifrar(message, key);
                 break;
             case DESXIFRAR:
-                res = p1.desxifrar(message, key);
+                //      res = p1.desxifrar(message, key);
                 break;
             default:
                 res = "";
         }
-        System.out.println("Resultat:"+res);
+        //   System.out.println("Resultat:" + res);
+    }
+
+    private static void example() {
+        try {
+            KeyGenerator keygen = KeyGenerator.getInstance("AES");
+            SecretKey aesKey = keygen.generateKey();
+            Cipher aesCipher;
+
+            // Create the cipher
+            aesCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            // Initialize the cipher for encryption
+            aesCipher.init(Cipher.ENCRYPT_MODE, aesKey);
+
+            // Our cleartext
+            byte[] cleartext = "Este deberia ser el mensaje correcto".getBytes();
+
+            // Encrypt the cleartext
+            byte[] ciphertext = aesCipher.doFinal(cleartext);
+
+            // Initialize the same cipher for decryption
+            aesCipher.init(Cipher.DECRYPT_MODE, aesKey);
+
+            // Decrypt the ciphertext
+            byte[] cleartext1 = aesCipher.doFinal(ciphertext);
+
+            System.out.println("cleartext-> lenght:" + cleartext.length + " - " + new String(cleartext));
+            System.out.println("cleartext1-> lenght:" + cleartext1.length + " - " + new String(cleartext1));
+        } catch (Exception ex) {
+            System.out.println("Error");
+        }
+
     }
 }
